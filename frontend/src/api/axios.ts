a93d2294902cb6d5102f81017fe0fdcfe75e9e60
sau-mili <146661411+sau-mili@users.noreply.github.com>
2026-05-18 17:44:29 +0530

    Update API URL for production

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // baseURL: 'http://127.0.0.1:5000/api',
   baseURL: 'https://smart-leads-dashboard-cok8.onrender.com', // Make sure this matches your backend!
 });
 
